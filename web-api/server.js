a4ce681df999435b082c3d799d872929bde09273
Muinde Geofrey <muinde@mesozi.com>
2022-03-29 15:02:04 +0300

    add unit tests and include them in ci

--- web-api/server.js
+++ web-api/server.js
@@ -1,16 +1,22 @@
 'use strict';
 
+
 const express = require('express');
+const {getHellowMessage} = require("./controllers/greeting.controller")
+
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+
+exports.greet = (req, res) => {
+  res.send(getHellowMessage());
+}
+
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.get('/', this.greet);
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
